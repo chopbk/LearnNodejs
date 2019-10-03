d7ccbf29eee7eae47d86d691d54cabd6416556bd
chopbk <dinhtam94@gmail.com>
2019-10-04 00:48:39 +0700

    use ejs template engine in express

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var links = [
+    { href: 'https://www.facebook.com/ChopBK', text: 'Facebook Nguyễn Đình Tâm' },
+    { href: 'https://www.linkedin.com/in/dinhtam94/', text: 'Linkedin Nguyễn Đình Tâm' },
+    { href: 'https://viblo.asia/', text: 'Viblo' },
+    { href: '/', text: 'Text Link 1' },
+    { href: '/', text: 'Text Link 2' },
+    { href: '/', text: 'Text Link 3' },
+    { href: '/', text: 'Text Link 4' },
+  ];
+  var headline = 'Nguyễn Đình Tâm';
+  var tagline = "Tôi là một lập trình viên đang học Nodejs và Express"
+
+  res.render('index', {links: links, headline: headline, tagline: tagline, title: 'Express' });
 });
 
 module.exports = router;
